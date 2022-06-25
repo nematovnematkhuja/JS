3b01384fffc0f2386d162c9b6e4bce59af45c9ec
nematovnematkhuja <nnematov518@gmail.com>
2022-06-25 20:49:10 +0500

    JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+function addOn(){
+    let rasm = document.querySelector("img")
+    rasm.src = "img/yoniq.jpg"
+}
+
+function addOff(){
+    let rasm = document.querySelector("img")
+    rasm.src = "img/ochiq.jpg"
+}
